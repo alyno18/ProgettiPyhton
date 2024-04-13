61b5d95ac667b05206c368ddf6e314e793bb611a
alyno18 <alybonomi18@gmail.com>
2024-04-13 11:18:09 +0200

    13/04/2024

---
+++ esercitazione1/esercitazione1.py
@@ -0,0 +1,77 @@
+# Ex1: Scrivere un programma Python che elabora tre numeri e
+# ne mostra in console:
+#   La somma
+#   La differenza
+#   Il prodotto
+#   Il valore medio
+#   Il massimo
+#   Il minimo
+
+#a, b, c = input("Inserisci 3 numeri").split()
+a = 5
+b = 6
+c = 7
+print(a+b+c)
+print(a-b-c)
+print((a+b+c)/3)
+print(max(a, b, c))
+print(min(a, b, c))
+
+
+# Ex2: Scrivere un programma Python che elabora un intero
+# di 6 cifre e visualizza le singole cifre di cui è composto.
+# Ad esempio, avendo il numero 458932,
+# il programma deve visualizzare, su righe separate 4 5 8 9 3 2
+
+num = 5678
+
+for i in range(len(str(num))):
+    print(str(num)[i])
+
+
+# Ex3: Scrivere un programma che chiede all’utente di
+# inserire una stringa e mostra a schermo se:
+#   La stringa contiene almeno un numero
+#   La stringa contiene tutte lettere minuscole
+#   La stringa contiene soltanto numeri
+#   La stringa contiene soltanto lettere e numeri
+
+#stringa = input("Inserisci una stringa")
+stringa = "ciao"
+
+print(stringa.isdigit())
+print(stringa.islower())
+print(stringa.isupper())
+print(stringa.isalnum())
+
+
+# Ex4: Scrivere un programma Python che chiede all’utente
+# di inserire quattro numeri e mostra in console la parola
+# “crescenti” se sono in ordine strettamente crescente,
+# “decrescenti” se sono in ordine strettamente decrescente e
+# “nessuno dei due” se non sono né in ordine crescente
+# né in ordine decrescente.
+
+sequenza = []
+n = int(input("Inserisci il numero di elementi che vuoi inserire"))
+for i in range(0,n):
+    ele = int(input("Inserisci un numero"))
+    sequenza.append(ele)
+
+
+# Ex5: Scrivere un programma Python che chiede all’utente di
+# inserire un mese e un giorno e mostra in console
+# la stagione dell’anno (Inverno, primavera, estate, autunno)
+# relativa al mese e al giorno inseriti.
+
+mese = int(input("Inserisci il mese"))
+giorno = int(input("Inserisci il giorno"))
+
+if (mese == 3 and giorno >= 20) or (mese == 6 and giorno <= 21) or (mese == 4) or (mese == 5):
+    print("Primavera")
+elif (mese == 6 and giorno >= 21) or (mese == 9 and giorno <= 22) or (mese == 7) or (mese == 8):
+    print("Estate")
+elif (mese == 9 and giorno >= 23) or (mese == 12 and giorno <= 21) or (mese == 10) or (mese == 11):
+    print("Autunno")
+elif (mese == 12 and giorno >= 22) or (mese == 3 and giorno <= 20) or (mese == 1) or (mese == 2):
+    print("Inverno")
---
+++ firstNumpy/firstNumpy.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+lista1 = [1, 2, 3, 4]
+tupla1 = (1, 2, 3, 4)
+b = np.array(lista1)
+c = np.array(tupla1)
+a1 = np.zeros(shape=(3, 3))
+print("a1: ", a1)
+b1 = np.ones(shape=(2, 2))
+print("b1:", b1)
+c1 = np.diag(b1)
+print("c1: ", c1)
+d1 = np.empty(shape=(3, 3))
+print("d1: ", d1)
+e1 = np.eye(4) #np.identity
+print("e1: ", e1)
